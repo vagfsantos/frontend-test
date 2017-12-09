965845422a680284f112c9e0994f8a67021b4469
Vagner <santos.vagnerferreira@gmail.com>
2017-12-09 14:20:12 -0300

    overall structure of fazenda ranking widget

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FazendaRankingComponent } from './fazenda-ranking/fazenda-ranking.component';
+import { HeaderComponent } from './fazenda-ranking/header/header.component';
+import { CandidateComponent } from './fazenda-ranking/candidate/candidate.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FazendaRankingComponent
+    FazendaRankingComponent,
+    HeaderComponent,
+    CandidateComponent
   ],
   imports: [
     BrowserModule
