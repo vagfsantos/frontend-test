73a9d91b3a780381f546f153f12d641202dd06c5
Vagner <santos.vagnerferreira@gmail.com>
2017-12-09 14:15:24 -0300

    init fazenda ranking component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FazendaRankingComponent } from './fazenda-ranking/fazenda-ranking.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FazendaRankingComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/fazenda-ranking/fazenda-ranking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fazenda-ranking',
+  templateUrl: './fazenda-ranking.component.html',
+  styleUrls: ['./fazenda-ranking.component.scss']
+})
+export class FazendaRankingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
